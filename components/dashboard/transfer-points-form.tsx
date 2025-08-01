e85f95e56df3c27f527dc779bc98f20a91b3ec23
FODreward <fridayogorchukwudaniel@gmail.com>
2025-08-01 13:10:02 +0100

    Update transfer-points-form.tsx

--- components/dashboard/transfer-points-form.tsx
+++ components/dashboard/transfer-points-form.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import type React from "react"
-import { useToast } from "@/components/ui/use-toast"
+import { useToast } from "@/hooks/use-toast"
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
