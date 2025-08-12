db6df9e42a127efd31cec850a03c03659a73a48f
FODreward <fridayogorchukwudaniel@gmail.com>
2025-08-12 08:33:03 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -535,7 +535,7 @@ export default function HomePage() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 Survecta. All rights reserved.</p>
+            <p>&copy; 2025 Survecta. All rights reserved.</p>
           </div>
         </div>
       </footer>
