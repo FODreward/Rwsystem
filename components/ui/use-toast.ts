1b2492a575c822279c921e7647ec641789d6e4e7
FODreward <fridayogorchukwudaniel@gmail.com>
2025-08-12 22:20:23 +0100

    Update use-toast.ts

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -5,7 +5,7 @@ import * as React from "react"
 
 import type { ToastActionElement, ToastProps } from "@/components/ui/toast"
 
-const TOAST_LIMIT = 3
+const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 300
 const TOAST_AUTO_DISMISS_DELAY = 500
 
